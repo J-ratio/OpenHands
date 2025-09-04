116c9cb461d5e2877605ade147ef727668605159
Dinesh Bala <baladnes@gmail.com>
2025-09-04 21:25:35 +0545

    fix: workspace select bugs on file manager

--- frontend/src/components/features/documents/_components/WorkspaceSelect.jsx
+++ frontend/src/components/features/documents/_components/WorkspaceSelect.jsx
@@ -12,6 +12,7 @@ import { useEffect, useState } from "react";
 import CreateWorkspaceButton from "../../../features/workspaces/components/create-workspace-button";
 import { getWorkspaceNameFromId } from "../../../../utils/workspace-utils";
 import { getColorFromName } from "../../../../utils/basic-utils";
+import { useSettings } from "../../../../hooks/query/use-settings";
 
 export const WorkspaceSelect = ({
   workspaces,
@@ -25,6 +26,7 @@ export const WorkspaceSelect = ({
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const { data: settings } = useSettings();
 
   async function getData() {
     setLoading(true);
@@ -33,12 +35,12 @@ export const WorkspaceSelect = ({
       setData(response.data);
       if (workspaces) workspaces = response.data;
       if (setWorkspaces) setWorkspaces(response.data);
-      if (!selectedWorkspace && workspaces) {
-        setSelectedWorkspace(workspaces[0].id.toString());
-        if (setSelectedWorkspace) {
-          setSelectedWorkspaceName(workspaces[0].name);
-        }
-      }
+      // if (!selectedWorkspace && workspaces) {
+      //   setSelectedWorkspace(workspaces[0].id.toString());
+      //   if (setSelectedWorkspace) {
+      //     setSelectedWorkspaceName(workspaces[0].name);
+      //   }
+      // }
     } else {
       setError(response.errorMessage);
     }
@@ -48,6 +50,13 @@ export const WorkspaceSelect = ({
   useEffect(() => {
     getData();
   }, []);
+
+  useEffect(() => {
+    if (!selectedWorkspace && workspaces) {
+      const activeWorkspaceId = settings?.ACTIVE_WORKSPACE_ID;
+      setSelectedWorkspace(activeWorkspaceId);
+    }
+  }, [settings]);
 
   if (loading) return <div className="text-neutral-400"></div>;
   if (error) {
