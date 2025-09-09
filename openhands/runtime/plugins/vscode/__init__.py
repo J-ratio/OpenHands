becebb30e4bde93b0f5526a2af3c4e556e86e2c6
Dinesh Bala <baladnes@gmail.com>
2025-09-09 12:18:49 +0545

    feat: add to chat vscode extension

--- openhands/runtime/plugins/vscode/__init__.py
+++ openhands/runtime/plugins/vscode/__init__.py
@@ -134,17 +134,7 @@ class VSCodePlugin(Plugin):
         # Make sure the settings file is readable and writable by all users
         os.chmod(target_path, 0o666)
 
-        extensions_src = current_dir / "extensions"
-        extensions_dest = Path("/openhands/.openvscode-server/extensions")
-
-        if extensions_src.exists():
-            shutil.copytree(
-                extensions_src,
-                extensions_dest,
-                dirs_exist_ok=True
-            )
-
-        logger.debug(f'VSCode settings and extensions synced to {extensions_dest}')
+        logger.debug(f'VSCode settings copied to {target_path}')
 
     async def run(self, action: Action) -> Observation:
         """Run the plugin for a given action."""
